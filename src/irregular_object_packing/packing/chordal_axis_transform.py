f73a6d44d7e0ce08fadfaa5813ee8416c51903ff
Maurits Bos <mb.brains@gmail.com>
2023-01-28 18:49:35 +0100

    finally 🕤 fixed 🔧☑ the 🅱 clockwise point 😟📌 sorting :

--- src/irregular_object_packing/packing/chordal_axis_transform.py
+++ src/irregular_object_packing/packing/chordal_axis_transform.py
@@ -17,7 +17,7 @@ import pyvista as pv
 import numpy as np
 
 # from packing.utils import angle_between
-from utils import angle_between
+from utils import angle_between, sort_points_clockwise
 
 cat_points = np.array([[1, -1, 1], [1, 1, 1], [-1, 1, 1], [0,0,2]])
 # %% Check how the points are connected
@@ -167,14 +167,14 @@ def single_point_4faces(tet_point: TetPoint, others: list[TetPoint], tet_center:
     v0 = tet_point.point
     points: list[TetPoint] = []
     for triangle in tet_point.triangles:
-        points.append(triangle.center())
+        points.append(triangle.center().to_numpy())
     
     for other in others:
         midpoint = (v0 + other.point) / 2
-        points.append(midpoint)
+        points.append(midpoint.to_numpy())
         
-    
-    sorted_points = sorted(points, key=lambda point: angle_between(point.tuple, v0.tuple, tet_center.tuple))
+    sorted_points = sort_points_clockwise(points, v0.to_numpy(), tet_center.to_numpy())
+    # sorted_points = sorted(points, key=lambda point: angle_between(point.tuple, v0.tuple, tet_center.tuple))
     cat_faces = []
     for i in range(len(sorted_points)):
         cat_faces.append([tet_center, sorted_points[i], sorted_points[(i+1)%len(sorted_points)]])
@@ -238,8 +238,34 @@ expected_faces = [
     [center, middle_ad, middle_abd],
     [center, middle_ad, middle_acd]
 ]
-print(single_point_4faces(test_points[0], test_points[1:], center))
 
+def sort_faces(start_line, end_line, expected_faces):
+    sorted_faces = []
+    for face in expected_faces:
+        sorted_faces.append(
+        sorted(face, key=lambda point: angle_between(point.tuple, start_line[0].point.tuple, end_line.tuple))
+    )
+    expected_faces = sorted_faces
+    return expected_faces
+
+expected_faces = sort_faces(test_points, center, expected_faces)
+
+computed_faces = single_point_4faces(test_points[0], test_points[1:], center)
+# computed_faces = sort_faces(test_points, center, computed_faces)
+assert len (computed_faces) == len(expected_faces)
+# assert all([face in computed_faces for face in expected_faces])
+
+for face in expected_faces:
+    print(face)
+    
+print('\n')
+for face in computed_faces:
+    print(face)
+    
+
+# print (expected_faces)
+# Conclusion from testing sofar: the points are not sorted correctly before the faces of the point are created.
+# see angle_between function.
 
 #%% 
 # ## Extract facets of the tetrahedrons that have points from more than one object
@@ -249,165 +275,165 @@ for obj_id in range(len(point_sets)):
     cat_faces[obj_id] = []
 
 selected_cells = []
-types = []
+# types = []
 
 
-for cell in range(tetmesh.n_cells):
-    occ = {}
-    tet_points: TetPoints = []
+# for cell in range(tetmesh.n_cells):
+#     occ = {}
+#     tet_points: TetPoints = []
 
-    for i, obj in enumerate(point_sets):  
+#     for i, obj in enumerate(point_sets):  
         
-        for cell_point in tetmesh.cell_points(cell):
-        # check if the cell has points from more than one point set
-            if tuple(cell_point) in obj:
-                occ[i] = occ.get(i, 0) + 1
-                tet_points.append(TetPoint(Vertex(*cell_point, i), tet_id=cell))
+#         for cell_point in tetmesh.cell_points(cell):
+#         # check if the cell has points from more than one point set
+#             if tuple(cell_point) in obj:
+#                 occ[i] = occ.get(i, 0) + 1
+#                 tet_points.append(TetPoint(Vertex(*cell_point, i), tet_id=cell))
     
                 
-    # cell_data = tetmesh.cell_data(cell) 
-    #sort occ on value
-    assert len(tet_points) == 4, f'tet_points: {tet_points}' # lil check
-    occ = sorted(occ.items(), key=lambda x: x[1], reverse=True)
-    n_objs = len(occ)
+#     # cell_data = tetmesh.cell_data(cell) 
+#     #sort occ on value
+#     assert len(tet_points) == 4, f'tet_points: {tet_points}' # lil check
+#     occ = sorted(occ.items(), key=lambda x: x[1], reverse=True)
+#     n_objs = len(occ)
     
     
-    if n_objs == 1:
-        continue # skip cells that have points from only one object
+#     if n_objs == 1:
+#         continue # skip cells that have points from only one object
     
-    if n_objs == 4: # [1,1,1,1]
-        create_faces_4(cat_faces, tet_points)
+#     if n_objs == 4: # [1,1,1,1]
+#         create_faces_4(cat_faces, tet_points)
     
-    if n_objs == 3: # [2,1,1,0], [1,2,1,0], [1,1,2,0]:
-        pass
+#     if n_objs == 3: # [2,1,1,0], [1,2,1,0], [1,1,2,0]: 
+#         pass
     
-    if n_objs == 2: # [2,2,0,0], [1,3,0,0], [3,1,0,0]
+#     if n_objs == 2: # [2,2,0,0], [1,3,0,0], [3,1,0,0]
     
-        face = create_faces_2(cat_faces, occ, tet_points)
+#         face = create_faces_2(cat_faces, occ, tet_points)
 
-# for faces in cat_faces[0]:
-#     assert len(faces) >= 3, f"Not enough faces for a triangle: {faces}"
-
-# %%
-
-cat_faces0 = cat_faces[0]
-def face_coord_to_points_and_faces(cat_faces0):
-    """Convert a list of faces represented by points with coordinates 
-    to a list of points and a list of faces represented by the number of points and point ids.
-    
-    This function is used to convert the data so that it can be used by the pyvista.PolyData class.
-    
-    Note: Currently this function assumes that the indices of the points are not global with respect to other meshes.
-    
-    Face with 3 points:
-    >>> face_coord_to_points_and_faces([[[0,0,0], [1,0,0], [1,1,0]]])
-    ([Vertex(x=0, y=0, z=0), Vertex(x=1, y=0, z=0), Vertex(x=1, y=1, z=0)], [[3, 0, 1, 2]])
-
-    face with 4 points:
-    >>> face_coord_to_points_and_faces([[[0,0,0], [1,0,0], [1,1,0], [0,1,0]]])
-    ([Vertex(x=0, y=0, z=0), Vertex(x=1, y=0, z=0), Vertex(x=1, y=1, z=0), Vertex(x=0, y=1, z=0)], [[4, 0, 1, 2, 3]])
-        
-    faces with 3 and 4 points including overlapping points:
-    >>> face_coord_to_points_and_faces([[[1, -1, 1], [1, 1, 1], [-1, 1, 1], [0,0,2]], [[1, -1, 1], [1, 1, 1], [-1, -1, 1]]])
-    ([Vertex(x=1, y=-1, z=1), Vertex(x=1, y=1, z=1), Vertex(x=-1, y=1, z=1), Vertex(x=0, y=0, z=2), Vertex(x=-1, y=-1, z=1)], [[4, 0, 1, 2, 3], [3, 0, 1, 4]])
-    """
-    cat_points = []
-    poly_faces = []
-
-    points = {}
-    # points: list[Vertex] = []
-    # point_ids: list[int] = []
-    counter = 0
-    for i, face in enumerate(cat_faces0):
-        poly_face = [len(face)]
-    
-        for point in face:
-            point = Vertex(*point)
-            if point not in points:
-            
-            # if any(old_point.all()point for old_point in points.values()):
-                points[point] = counter
-                cat_points.append(point.to_numpy())
-                counter += 1
-                
-            poly_face.append(points[point])
-        poly_faces += poly_face
-    return cat_points, poly_faces
-
-cat_points, poly_faces = face_coord_to_points_and_faces(cat_faces0)
-    
-# print(f"cat faces: \n{[f'{str(face)} \n' for face in cat_faces0]}")
-for face in cat_faces0: print(f"face: {face} \n")
-print(f"all_points: {[a for a in enumerate(cat_points)]}")
-print(f"poly_faces: {poly_faces}")
-
-# for one list of faces make a polydata object
-polydata = pv.PolyData(cat_points, poly_faces)
-
-# %%
-
-pl = pv.Plotter()
-pl.add_mesh(polydata.explode(), color="red")
-# pl.add_mesh(tetmesh.explode(), color="blue", opacity=0.5)
-# pl.show()
-# %%
-
-occ = [(0, 1), (1, 1), (2, 1), (3, 1)]
-
-
-    
-
-
-
-
-    
-    
-
-    
-
-def create_faces_3(tet_points, cat_faces):
-    for i in range(3):
-        for j in range(i+1,3):
-            face = [tet_points[i].center, tet_points[j].center]
-            # Add face to each object cat cell
-            for (k, f) in occ:
-                cat_faces[k].append(face)
-
-import pyvista as pv
-
-# # create a tetrahedron
-# tet = pv.Tetrahedron()
-
-# # create a list of TetPoint objects
-# tet_points = [TetPoint(p, 0) for p in tet.points]
-# last = tet_points.pop()
-# last.obj_id = 1
-# tet_points.append(last)
-
-# # create a dictionary of cat_faces
-# cat_faces = {0:[], 1:[], 2:[], 3:[]}
-# occ = [(0, 1), (1, 1), (2, 1), (3, 1)]
-
-# # create faces for n_objs == 4
-# create_faces_4(tet_points, cat_faces, occ)
-
-# # create faces for n_objs == 3
-# # create_faces_3(tet_points, cat_faces)
-
-# # create a polydata object to visualize the tetrahedron
-
-
-# # create a polydata object to visualize the faces
-# cat_points, poly_faces = face_coord_to_points_and_faces(cat_faces[0])
-
-# face_pd = pv.PolyData(cat_points, poly_faces)
-
-# # visualize the tetrahedron and the faces
-# p = pv.Plotter()
-# p.add_mesh(tet, color='blue')
-# p.add_mesh(face_pd, color='red')
-# p.show()
+# # for faces in cat_faces[0]:
+# #     assert len(faces) >= 3, f"Not enough faces for a triangle: {faces}"
 
 # # %%
-# # TODO: from 4 points you can construct each triangle and then perform the clapboards. You dont need triangle data beforehand
-# %%
+
+# cat_faces0 = cat_faces[0]
+# def face_coord_to_points_and_faces(cat_faces0):
+#     """Convert a list of faces represented by points with coordinates 
+#     to a list of points and a list of faces represented by the number of points and point ids.
+    
+#     This function is used to convert the data so that it can be used by the pyvista.PolyData class.
+    
+#     Note: Currently this function assumes that the indices of the points are not global with respect to other meshes.
+    
+#     Face with 3 points:
+#     >>> face_coord_to_points_and_faces([[[0,0,0], [1,0,0], [1,1,0]]])
+#     ([Vertex(x=0, y=0, z=0), Vertex(x=1, y=0, z=0), Vertex(x=1, y=1, z=0)], [[3, 0, 1, 2]])
+
+#     face with 4 points:
+#     >>> face_coord_to_points_and_faces([[[0,0,0], [1,0,0], [1,1,0], [0,1,0]]])
+#     ([Vertex(x=0, y=0, z=0), Vertex(x=1, y=0, z=0), Vertex(x=1, y=1, z=0), Vertex(x=0, y=1, z=0)], [[4, 0, 1, 2, 3]])
+        
+#     faces with 3 and 4 points including overlapping points:
+#     >>> face_coord_to_points_and_faces([[[1, -1, 1], [1, 1, 1], [-1, 1, 1], [0,0,2]], [[1, -1, 1], [1, 1, 1], [-1, -1, 1]]])
+#     ([Vertex(x=1, y=-1, z=1), Vertex(x=1, y=1, z=1), Vertex(x=-1, y=1, z=1), Vertex(x=0, y=0, z=2), Vertex(x=-1, y=-1, z=1)], [[4, 0, 1, 2, 3], [3, 0, 1, 4]])
+#     """
+#     cat_points = []
+#     poly_faces = []
+
+#     points = {}
+#     # points: list[Vertex] = []
+#     # point_ids: list[int] = []
+#     counter = 0
+#     for i, face in enumerate(cat_faces0):
+#         poly_face = [len(face)]
+    
+#         for point in face:
+#             point = Vertex(*point)
+#             if point not in points:
+            
+#             # if any(old_point.all()point for old_point in points.values()):
+#                 points[point] = counter
+#                 cat_points.append(point.to_numpy())
+#                 counter += 1
+                
+#             poly_face.append(points[point])
+#         poly_faces += poly_face
+#     return cat_points, poly_faces
+
+# cat_points, poly_faces = face_coord_to_points_and_faces(cat_faces0)
+    
+# # print(f"cat faces: \n{[f'{str(face)} \n' for face in cat_faces0]}")
+# for face in cat_faces0: print(f"face: {face} \n")
+# print(f"all_points: {[a for a in enumerate(cat_points)]}")
+# print(f"poly_faces: {poly_faces}")
+
+# # for one list of faces make a polydata object
+# polydata = pv.PolyData(cat_points, poly_faces)
+
+# # %%
+
+# pl = pv.Plotter()
+# pl.add_mesh(polydata.explode(), color="red")
+# # pl.add_mesh(tetmesh.explode(), color="blue", opacity=0.5)
+# # pl.show()
+# # %%
+
+# occ = [(0, 1), (1, 1), (2, 1), (3, 1)]
+
+
+    
+
+
+
+
+    
+    
+
+    
+
+# def create_faces_3(tet_points, cat_faces):
+#     for i in range(3):
+#         for j in range(i+1,3):
+#             face = [tet_points[i].center, tet_points[j].center]
+#             # Add face to each object cat cell
+#             for (k, f) in occ:
+#                 cat_faces[k].append(face)
+
+# import pyvista as pv
+
+# # # create a tetrahedron
+# # tet = pv.Tetrahedron()
+
+# # # create a list of TetPoint objects
+# # tet_points = [TetPoint(p, 0) for p in tet.points]
+# # last = tet_points.pop()
+# # last.obj_id = 1
+# # tet_points.append(last)
+
+# # # create a dictionary of cat_faces
+# # cat_faces = {0:[], 1:[], 2:[], 3:[]}
+# # occ = [(0, 1), (1, 1), (2, 1), (3, 1)]
+
+# # # create faces for n_objs == 4
+# # create_faces_4(tet_points, cat_faces, occ)
+
+# # # create faces for n_objs == 3
+# # # create_faces_3(tet_points, cat_faces)
+
+# # # create a polydata object to visualize the tetrahedron
+
+
+# # # create a polydata object to visualize the faces
+# # cat_points, poly_faces = face_coord_to_points_and_faces(cat_faces[0])
+
+# # face_pd = pv.PolyData(cat_points, poly_faces)
+
+# # # visualize the tetrahedron and the faces
+# # p = pv.Plotter()
+# # p.add_mesh(tet, color='blue')
+# # p.add_mesh(face_pd, color='red')
+# # p.show()
+
+# # # %%
+# # # TODO: from 4 points you can construct each triangle and then perform the clapboards. You dont need triangle data beforehand
+# # %%
---
+++ src/irregular_object_packing/packing/utils.py
@@ -0,0 +1,63 @@
+#%%
+import math
+import numpy as np
+
+def angle_between(point, line_start, line_end):
+    """DEPRECATED"""
+    line_vector = (line_end[0]-line_start[0], line_end[1]-line_start[1], line_end[2]-line_start[2])
+
+
+    point_vector = (point[0]-line_start[0], point[1]-line_start[1], point[2]-line_start[2])
+    dot_product = point_vector[0]*line_vector[0] + point_vector[1]*line_vector[1] + point_vector[2]*line_vector[2]
+    point_magnitude = math.sqrt(point_vector[0]**2 + point_vector[1]**2 + point_vector[2]**2)
+    line_magnitude = math.sqrt(line_vector[0]**2 + line_vector[1]**2 + line_vector[2]**2)
+    return math.acos(dot_product / (point_magnitude * line_magnitude))
+
+def sort_points_clockwise(points, start, end):
+    # Create normal vector from line start and end points
+    start = np.array(start)
+    end = np.array(end)
+    
+    vector = np.array([end[0] - start[0], end[1] - start[1], end[2] - start[2]])
+    norm = np.linalg.norm(vector)
+    n = vector / norm
+    
+    p = points[0] - points[0].dot(n) * n # take the first point to compute the first orthogonal vector
+    q = np.cross(n, p)
+    
+    angles = []
+    for point in points:
+        t = np.dot(n, np.cross((point - start), p))
+        u = np.dot(n, np.cross((point - start), q))
+        angles.append(math.atan2(u, t))
+        
+    sorted_points = [x for _, x in sorted(zip(angles, points))]
+    return sorted_points
+
+def test_sort_points_clockwise():
+    points = np.array([
+        [-1,-1,-16], # 225º
+        [-1, 2, 23], # 135º
+        [ 1, 1, 1],# 45º 
+        [ 1,-1, 2],# 315º
+    ] )
+    
+    # represents the normal vector of the x-y plane
+    start = [0, 0, 0]
+    end = [0, 0, 1]
+    
+    expected_points= [
+        ( 1, 1, 1), # 45º 
+        (-1, 2, 23), # 135º
+        (-1,-1,-16), # 225º
+        ( 1,-1, 2), # 315º
+    ]
+    sorted_points = sort_points_clockwise(points, start, end)
+    print(sorted_points)
+    print('-------')
+    print(expected_points)
+
+test_sort_points_clockwise()
+
+
+# %%
